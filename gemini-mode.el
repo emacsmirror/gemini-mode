5a60ca31b3d8847be3b2057d1f28e047b594fa12
Étienne Deparis <etienne@depar.is>
2020-11-20 21:35:56 +0100

    Load visual-fill-column if it is available

--- gemini-mode.el
+++ gemini-mode.el
@@ -138,7 +138,7 @@ insert a list item."
   "Major mode for editing text/gemini 'geminimap' documents"
   (setq font-lock-defaults '(gemini-highlights))
   (visual-line-mode 1)
-  (when (featurep 'visual-fill-column)
+  (when (require 'visual-fill-column nil t)
     (visual-fill-column-mode 1)))
 
 ;;;###autoload
